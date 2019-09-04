902044a80ab9bceb1114ff454e8c0e3d3b1057f1
Evrei-XD <rom_ser2011@mail.ru>
2019-09-04 15:43:33 +0300

    15:38 незначительные доработки

--- app/src/main/java/me/Romans/motorica/ui/chat/view/Gripper_settings/FragmentGripperSettings.java
+++ app/src/main/java/me/Romans/motorica/ui/chat/view/Gripper_settings/FragmentGripperSettings.java
@@ -75,44 +75,24 @@ public class FragmentGripperSettings extends Fragment implements ChatView {
         return view;
     }
 
-
     @Override
-    public void setStatus(String status) {
-
-    }
+    public void setStatus(String status) { }
     @Override
-    public void setStatus(int resId) {
-
-    }
+    public void setStatus(int resId) { }
     @Override
-    public void setValueCH(int levelCH, int numberChannel) {
-
-    }
+    public void setValueCH(int levelCH, int numberChannel) { }
     @Override
-    public void setErrorReception(boolean incomeErrorReception) {
-
-    }
+    public void setErrorReception(boolean incomeErrorReception) { }
     @Override
-    public void appendMessage(String message) {
-
-    }
+    public void appendMessage(String message) { }
     @Override
-    public void enableHWButton(boolean enabled) {
-
-    }
+    public void enableHWButton(boolean enabled) { }
     @Override
-    public void showToast(String message) {
-
-    }
+    public void showToast(String message) { }
     @Override
-    public void onGestureClick(int position) {
-    }
+    public void onGestureClick(int position) { }
     @Override
-    public void setGeneralValue(int receiveСurrent, int receiveLevelCH1, int receiveLevelCH2, byte receiveIndicationState, int receiveBatteryTension) {
-
-    }
+    public void setGeneralValue(int receiveСurrent, int receiveLevelCH1, int receiveLevelCH2, byte receiveIndicationState, int receiveBatteryTension) { }
     @Override
-    public void setStartParameters(Integer receiveСurrent, Integer receiveLevelTrigCH1, Integer receiveLevelTrigCH2, Byte receiveIndicationInvertMode, Byte receiveBlockIndication) {
-
-    }
+    public void setStartParameters(Integer receiveСurrent, Integer receiveLevelTrigCH1, Integer receiveLevelTrigCH2, Byte receiveIndicationInvertMode, Byte receiveBlockIndication) { }
 }

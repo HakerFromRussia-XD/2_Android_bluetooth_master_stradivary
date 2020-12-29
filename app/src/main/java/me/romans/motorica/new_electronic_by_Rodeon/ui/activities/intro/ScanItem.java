be27ebff526fd6aa174b536ae56059dd9dd84a58
Evrei-XD <rom_ser2011@mail.ru>
2020-12-29 17:36:33 +0300

    17:35 Добавленны ненужнае файлы, дря решения конфликтоа при слиянии веток

---
+++ app/src/main/java/me/romans/motorica/new_electronic_by_Rodeon/ui/activities/intro/ScanItem.java
@@ -0,0 +1,19 @@
+package me.romans.motorica.new_electronic_by_Rodeon.ui.activities.intro;
+
+public class ScanItem {
+    private int image;
+    private String title;
+    private boolean checkProgress;
+
+    public ScanItem(int image, String title, boolean checkProgress) {
+        this.image = image;
+        this.title = title;
+        this.checkProgress = checkProgress;
+    }
+
+    public int getImage() {
+        return image;
+    }
+    public String getTitle() { return title; }
+    public boolean getCheckProgress() { return checkProgress; }
+}
---
+++ app/src/main/java/me/romans/motorica/new_electronic_by_Rodeon/ui/activities/intro/ScanView.java
@@ -0,0 +1,8 @@
+package me.romans.motorica.new_electronic_by_Rodeon.ui.activities.intro;
+
+import android.bluetooth.BluetoothDevice;
+
+public interface ScanView {
+    void addDeviceToScanList(String item, BluetoothDevice device);
+    void clearScanList();
+}

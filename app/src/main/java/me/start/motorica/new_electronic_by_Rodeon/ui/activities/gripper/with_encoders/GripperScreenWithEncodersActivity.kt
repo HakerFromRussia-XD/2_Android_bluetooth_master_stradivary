08b2532ad1836b92a498c6500de53535ef2ca206
Evrei-XD <rom_ser2011@mail.ru>
2021-08-20 14:24:31 +0300

    Реализована возможность появления и скрытия клавиатуры и эдит текста при включении и выключении режима редактирования названия жеста

--- app/src/main/java/me/start/motorica/new_electronic_by_Rodeon/ui/activities/gripper/with_encoders/GripperScreenWithEncodersActivity.kt
+++ app/src/main/java/me/start/motorica/new_electronic_by_Rodeon/ui/activities/gripper/with_encoders/GripperScreenWithEncodersActivity.kt
@@ -1,12 +1,16 @@
 package me.start.motorica.new_electronic_by_Rodeon.ui.activities.gripper.with_encoders
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.ActivityManager
+import android.app.Service
 import android.content.Context
 import android.graphics.PixelFormat
 import android.os.Bundle
+import android.text.InputType
 import android.util.DisplayMetrics
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.SeekBar
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -42,14 +46,21 @@ class GripperScreenWithEncodersActivity
         RxView.clicks(findViewById(R.id.edit_gesture_name_btn))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
+                    val imm = this.getSystemService(Service.INPUT_METHOD_SERVICE) as InputMethodManager
                     if (editMode) {
                         edit_gesture_name_btn.setImageResource(R.drawable.ic_edit_24)
-                        gesture_name_tv.visibility = View.GONE
-                        gesture_name_et.visibility = View.VISIBLE
+                        gesture_name_tv.visibility = View.VISIBLE
+                        imm.hideSoftInputFromWindow (this.currentFocus?.windowToken, 0)
+                        gesture_name_et.visibility = View.GONE
                         editMode = false
                     } else {
                         edit_gesture_name_btn.setImageResource(R.drawable.ic_cancel_24)
                         gesture_name_tv.visibility = View.GONE
+                        gesture_name_et.visibility = View.VISIBLE
+                        gesture_name_et.requestFocus()
+                        imm.hideSoftInputFromWindow(gesture_name_et.windowToken, 0)
+                        imm.showSoftInput(gesture_name_et, 0);
+                        gesture_name_et.isFocusableInTouchMode = true
                         editMode = true
                     }
                 }

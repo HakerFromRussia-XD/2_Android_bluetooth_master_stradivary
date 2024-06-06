ee2457a9cc5fbc1647da2bdcf42d33df2c8bf412
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2024-06-06 20:05:31 +0300

    Добавлен первый unit test

---
+++ app/src/main/java/com/bailout/stickk/new_electronic_by_Rodeon/utils/NameUtilTest.kt
@@ -0,0 +1,12 @@
+package com.bailout.stickk.new_electronic_by_Rodeon.utils
+
+
+import org.junit.jupiter.api.Test
+
+class NameUtilTest {
+
+    @Test
+    fun getCleanName() {
+        assert(false)
+    }
+}

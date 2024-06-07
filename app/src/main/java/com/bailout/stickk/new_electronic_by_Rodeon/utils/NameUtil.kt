a8a2fad8506cc9974e7c087a23dd231edfc8f0f5
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2024-06-07 14:28:50 +0300

    NameUtil покрыт тестом

--- app/src/main/java/com/bailout/stickk/new_electronic_by_Rodeon/utils/NameUtil.kt
+++ app/src/main/java/com/bailout/stickk/new_electronic_by_Rodeon/utils/NameUtil.kt
@@ -5,14 +5,13 @@ import com.bailout.stickk.new_electronic_by_Rodeon.ble.ConstantManager
 object NameUtil {
     fun getCleanName(deviceName: String): String {
         if (deviceName.contains(ConstantManager.DEVICE_TYPE_FEST_X)) {
-            if (deviceName.length > 6) {
+            if ( deviceName.length == 15 ) {
                 if (deviceName.contains(" ")) {
                     return deviceName
                 }
                 var newName = ""
                 val namePrefix = deviceName.substring(6, 10)
                 val nameCode = deviceName.substring(10, deviceName.length)
-//                if (deviceName.length == 13) { nameCode = deviceName.substring(11, deviceName.length) }
                 System.err.println("Test getCleanName() deviceName: $deviceName")
                 System.err.println("Test getCleanName() namePrefix: $namePrefix")
                 System.err.println("Test getCleanName() nameCode: $nameCode")
---
+++ app/src/test/java/com/bailout/stickk/new_electronic_by_Rodeon/utils/NameUtilTest.kt
@@ -0,0 +1,47 @@
+package com.bailout.stickk.new_electronic_by_Rodeon.utils
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class NameUtilTest {
+
+    @Test
+    fun `should return actual serial number from the encoded serial number`() {
+        var cleanName = NameUtil.getCleanName("FEST-XFTHS00001")
+        assertEquals("FEST-H-00001", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTHS000012")
+        assertEquals("FEST-XFTHS000012", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-X")
+        assertEquals("FEST-X", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XF")
+        assertEquals("FEST-XF", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFT")
+        assertEquals("FEST-XFT", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTH")
+        assertEquals("FEST-XFTH", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTHS")
+        assertEquals("FEST-XFTHS", cleanName)
+
+        cleanName = NameUtil.getCleanName("FEST-XFTHS0")
+        assertEquals("FEST-XFTHS0", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTHS00")
+        assertEquals("FEST-XFTHS00", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTHS000")
+        assertEquals("FEST-XFTHS000", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTHS0000")
+        assertEquals("FEST-XFTHS0000", cleanName)
+
+        cleanName = NameUtil.getCleanName("FEST-XFTFS00001")
+        assertEquals("FEST-F-00001", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTFO00001")
+        assertEquals("FEST-FO-00001", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTHO00001")
+        assertEquals("FEST-HO-00001", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTEP00001")
+        assertEquals("FEST-EP-00001", cleanName)
+        cleanName = NameUtil.getCleanName("FEST-XFTEB00001")
+        assertEquals("FEST-EB-00001", cleanName)
+    }
+
+
+}

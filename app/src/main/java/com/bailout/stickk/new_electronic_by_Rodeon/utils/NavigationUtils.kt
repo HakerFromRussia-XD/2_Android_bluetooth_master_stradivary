30f50cac6af47e54276308fdfd0fb9a14240a698
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2024-08-28 12:38:05 +0300

    Подключена aar-кой библиотека navigationbottom-bar'a

--- app/src/main/java/com/bailout/stickk/new_electronic_by_Rodeon/utils/NavigationUtils.kt
+++ app/src/main/java/com/bailout/stickk/new_electronic_by_Rodeon/utils/NavigationUtils.kt
@@ -18,7 +18,7 @@ import android.graphics.Color
 import androidx.core.content.ContextCompat
 import androidx.viewpager.widget.ViewPager
 
-//import com.gigamole.navigationtabbar.ntb.NavigationTabBar
+import com.gigamole.navigationtabbar.ntb.NavigationTabBar
 import com.bailout.stickk.R
 
 import java.util.ArrayList
@@ -26,65 +26,65 @@ import java.util.ArrayList
 object NavigationUtils {
   var showAdvancedSettings = false
 
-//  private fun getNavigationModels(mContext: Context): ArrayList<NavigationTabBar.Model> {
-//    val colors = mContext.resources.getStringArray(R.array.colors)
-//    val models = ArrayList<NavigationTabBar.Model>()//здесь можно настроить боттом навигэйшен бар
-//    if (showAdvancedSettings) {
-//      models.add(
-//              NavigationTabBar.Model.Builder(
-//                      ContextCompat.getDrawable(mContext, R.drawable.ic_gestures),
-//                      Color.parseColor(colors[1]))
-//                      .title(mContext.resources.getString(R.string.setting_up_gestures))
-//                      .badgeTitle("new")
-//                      .build()
-//      )
-//      models.add(
-//              NavigationTabBar.Model.Builder(
-//                      ContextCompat.getDrawable(mContext, R.drawable.ic_sensors),
-//                      Color.parseColor(colors[3]))
-//                      .title(mContext.resources.getString(R.string.sensor_settings))//"настройка механики")
-//                      .badgeTitle("new")
-//                      .build()
-//      )
-//      models.add(
-//              NavigationTabBar.Model.Builder(
-//                      ContextCompat.getDrawable(mContext, R.drawable.ic_mechanics),
-//                      Color.parseColor(colors[4]))
-//                      .title(mContext.resources.getString(R.string.special_settings))
-//                      .badgeTitle("new")
-//                      .build()
-//      )
-//    } else {
-//      models.add(
-//              NavigationTabBar.Model.Builder(
-//                      ContextCompat.getDrawable(mContext, R.drawable.ic_gestures),
-//                      Color.parseColor(colors[1]))
-//                      .title(mContext.resources.getString(R.string.setting_up_gestures))
-//                      .badgeTitle("new")
-//                      .build()
-//      )
-//      models.add(
-//              NavigationTabBar.Model.Builder(
-//                      ContextCompat.getDrawable(mContext, R.drawable.ic_sensors),
-//                      Color.parseColor(colors[3]))
-//                      .title(mContext.resources.getString(R.string.sensor_settings))//"настройка механики")
-//                      .badgeTitle("new")
-//                      .build()
-//      )
-//    }
-//    return models
-//  }
+  private fun getNavigationModels(mContext: Context): ArrayList<NavigationTabBar.Model> {
+    val colors = mContext.resources.getStringArray(R.array.colors)
+    val models = ArrayList<NavigationTabBar.Model>()//здесь можно настроить боттом навигэйшен бар
+    if (showAdvancedSettings) {
+      models.add(
+              NavigationTabBar.Model.Builder(
+                      ContextCompat.getDrawable(mContext, R.drawable.ic_gestures),
+                      Color.parseColor(colors[1]))
+                      .title(mContext.resources.getString(R.string.setting_up_gestures))
+                      .badgeTitle("new")
+                      .build()
+      )
+      models.add(
+              NavigationTabBar.Model.Builder(
+                      ContextCompat.getDrawable(mContext, R.drawable.ic_sensors),
+                      Color.parseColor(colors[3]))
+                      .title(mContext.resources.getString(R.string.sensor_settings))//"настройка механики")
+                      .badgeTitle("new")
+                      .build()
+      )
+      models.add(
+              NavigationTabBar.Model.Builder(
+                      ContextCompat.getDrawable(mContext, R.drawable.ic_mechanics),
+                      Color.parseColor(colors[4]))
+                      .title(mContext.resources.getString(R.string.special_settings))
+                      .badgeTitle("new")
+                      .build()
+      )
+    } else {
+      models.add(
+              NavigationTabBar.Model.Builder(
+                      ContextCompat.getDrawable(mContext, R.drawable.ic_gestures),
+                      Color.parseColor(colors[1]))
+                      .title(mContext.resources.getString(R.string.setting_up_gestures))
+                      .badgeTitle("new")
+                      .build()
+      )
+      models.add(
+              NavigationTabBar.Model.Builder(
+                      ContextCompat.getDrawable(mContext, R.drawable.ic_sensors),
+                      Color.parseColor(colors[3]))
+                      .title(mContext.resources.getString(R.string.sensor_settings))//"настройка механики")
+                      .badgeTitle("new")
+                      .build()
+      )
+    }
+    return models
+  }
 
-//  fun setComponents(context: Context, navigationTabBar: NavigationTabBar) {
-//      navigationTabBar.models = getNavigationModels(context)
-//      navigationTabBar.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-//          override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
-//
-//          override fun onPageSelected(position: Int) {
-//              navigationTabBar.models[position].hideBadge()
-//          }
-//
-//          override fun onPageScrollStateChanged(state: Int) {}
-//      })
-//  }
+  fun setComponents(context: Context, navigationTabBar: NavigationTabBar) {
+      navigationTabBar.models = getNavigationModels(context)
+      navigationTabBar.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+          override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+          override fun onPageSelected(position: Int) {
+              navigationTabBar.models[position].hideBadge()
+          }
+
+          override fun onPageScrollStateChanged(state: Int) {}
+      })
+  }
 }

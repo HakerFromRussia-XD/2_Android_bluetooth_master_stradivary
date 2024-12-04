ae91d2e507f5032c6fbf76e294c3cb0fd77e5d40
Nikitin.V <torwards@yandex.com>
2024-12-04 22:24:15 +0300

    Множественный график с кнопками выбора и эффектами выбора канала

--- app/src/main/java/com/bailout/stickk/ubi4/adapters/widgetDelegeteAdapters/PlotDelegateAdapterMy.kt
+++ app/src/main/java/com/bailout/stickk/ubi4/adapters/widgetDelegeteAdapters/PlotDelegateAdapterMy.kt
@@ -1,10 +1,16 @@
 package com.bailout.stickk.ubi4.adapters.widgetDelegeteAdapters
 
+import android.R.attr.button
+import android.R.attr.padding
 import android.graphics.Color
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.widget.CompoundButton
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.marginTop
 import com.bailout.stickk.R
 import com.bailout.stickk.databinding.Ubi4WidgetPlotMyBinding
@@ -13,9 +19,9 @@ import com.bailout.stickk.ubi4.ble.BLECommands
 import com.bailout.stickk.ubi4.ble.ParameterProvider
 import com.bailout.stickk.ubi4.ble.SampleGattAttributes.MAIN_CHANNEL
 import com.bailout.stickk.ubi4.ble.SampleGattAttributes.WRITE
-import com.bailout.stickk.ubi4.models.PlotItem
 import com.bailout.stickk.ubi4.data.widget.endStructures.PlotParameterWidgetEStruct
 import com.bailout.stickk.ubi4.data.widget.endStructures.PlotParameterWidgetSStruct
+import com.bailout.stickk.ubi4.models.PlotItem
 import com.bailout.stickk.ubi4.persistence.preference.PreferenceKeysUBI4
 import com.bailout.stickk.ubi4.ui.main.MainActivityUBI4
 import com.bailout.stickk.ubi4.ui.main.MainActivityUBI4.Companion.countBinding
@@ -29,6 +35,7 @@ import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.livermor.delegateadapter.delegate.ViewBindingDelegateAdapter
+import com.simform.refresh.SSPullToRefreshLayout
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
@@ -36,6 +43,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+
 
 class PlotDelegateAdapterMy (
     val plotIsReadyToData:(num: Int) -> Unit) :
@@ -47,13 +55,18 @@ class PlotDelegateAdapterMy (
     private var dataSens4 = 0
     private var dataSens5 = 0
     private var dataSens6 = 0
-    private var numberOfCharts = 2
+    private var dataSenses: MutableList<Int> = ArrayList()
+    private var numberOfCharts = 5
 
     private var raw_data_set1: List<Entry> = ArrayList()
     private var g_data_sets: MutableList<LineDataSet> = ArrayList()
     private var g_data_set: LineDataSet = LineDataSet(null,null)
     private var invisible_top_data_set: LineDataSet = LineDataSet(null, null)
     private var invisible_bottom_data_set: LineDataSet = LineDataSet(null, null)
+
+    private var g_select_chart_buttons: MutableList<Button> = ArrayList()
+    private var g_selected_chart: Int = -1
+
 
     private var color: Int = 0
 
@@ -83,7 +96,8 @@ class PlotDelegateAdapterMy (
         if (PreferenceKeysUBI4.ParameterTypeEnum.entries[ParameterProvider.getParameter(deviceAddress, parameterID).type].sizeOf != 0){
             numberOfCharts = ParameterProvider.getParameter(deviceAddress, parameterID).parameterDataSize / PreferenceKeysUBI4.ParameterTypeEnum.entries[ParameterProvider.getParameter(deviceAddress, parameterID).type].sizeOf;
         } else {
-            numberOfCharts = 0
+//            numberOfCharts = 0 TODO:вернуть обратно
+            numberOfCharts = 5
         }
         Log.d("PlotDelegateAdapter", "numberOfCharts = $numberOfCharts parametrSize = ${ParameterProvider.getParameter(deviceAddress, parameterID).parameterDataSize}   type = ${ParameterProvider.getParameter(deviceAddress, parameterID).type}")
 
@@ -92,6 +106,20 @@ class PlotDelegateAdapterMy (
         initializedSensorGraph(EMGChartLc)
         graphThreadFlag = true
         countBinding += 1
+        for (i in 0..numberOfCharts-1){
+            val data_set: LineDataSet = LineDataSet(null,null)
+            data_set.lineWidth = 2f
+            data_set.color = Color.WHITE
+            data_set.mode = LineDataSet.Mode.LINEAR
+            data_set.setCircleColor(Color.TRANSPARENT)
+            data_set.circleHoleColor = Color.TRANSPARENT
+            data_set.fillColor = ColorTemplate.getHoloBlue()
+            data_set.highLightColor = Color.rgb(244, 117, 177)
+            data_set.valueTextColor = Color.TRANSPARENT
+            g_data_sets.add(data_set)
+
+            dataSenses.add(i,0)
+        }
         GlobalScope.launch(CoroutineName("startGraphEnteringDataCoroutine $countBinding")) {
             startGraphEnteringDataCoroutine(EMGChartLc)
         }
@@ -103,7 +131,57 @@ class PlotDelegateAdapterMy (
         color = main.applicationContext.getColor(R.color.open_threshold)
 
 
-//        g_data_set.axisDependency = YAxis.AxisDependency.LEFT
+        g_data_set.axisDependency = YAxis.AxisDependency.LEFT
+        for(i in 0..< numberOfCharts){
+            g_select_chart_buttons.add(i, Button(main))
+//            g_select_chart_buttons[i].layoutParams = LinearLayout(main).layoutParams
+
+            g_select_chart_buttons[i].background = AppCompatResources.getDrawable(main, R.drawable.ubi4_view_with_corners_gray)
+            g_select_chart_buttons[i].layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT, 1f)
+            val margin = (25 * main.resources.displayMetrics.density).toInt()
+            val param = g_select_chart_buttons[i].layoutParams as ViewGroup.MarginLayoutParams
+            if(i == 0 )
+                param.setMargins(20,20,10,10)
+            else if (i == numberOfCharts-1)
+                param.setMargins(10,20,20,10)
+            else
+                param.setMargins(10,20,10,10)
+            g_select_chart_buttons[i].setPadding(1,1,1,1);
+            g_select_chart_buttons[i].layoutParams = param
+            g_select_chart_buttons[i].text = (i+1).toString()
+            g_select_chart_buttons[i].setTextColor(Color.WHITE)
+            g_select_chart_buttons[i].textSize = 20f
+            g_select_chart_buttons[i].setOnClickListener {
+                if(i != g_selected_chart) {
+                    if(g_selected_chart != -1)
+                        g_select_chart_buttons[g_selected_chart].background =
+                            AppCompatResources.getDrawable(main, R.drawable.ubi4_view_with_corners_gray)
+                    g_select_chart_buttons[i].background = AppCompatResources.getDrawable(main, R.drawable.ubi4_view_with_corners_gray_active)
+                    g_selected_chart = i
+                }else{
+                    g_select_chart_buttons[i].background = AppCompatResources.getDrawable(main, R.drawable.ubi4_view_with_corners_gray)
+                    g_selected_chart = -1
+                }
+
+
+            }
+            topPanel.addView(g_select_chart_buttons[i])
+        }
+
+//        val mybtn = Button(main)
+//        mybtn.background = AppCompatResources.getDrawable(main, R.drawable.ubi4_view_with_corners_gray_active)
+//        mybtn.setOnClickListener {
+//            if( mybtn.background == AppCompatResources.getDrawable(main, R.drawable.ubi4_view_with_corners_gray_active) ){
+//                mybtn.background = AppCompatResources.getDrawable(main, R.drawable.ubi4_view_with_corners_gray)
+//            }else{
+//                mybtn.background = AppCompatResources.getDrawable(main, R.drawable.ubi4_view_with_corners_gray_active)
+//            }
+//        }
+
+//        topPanel.addView(mybtn)
+
+
         g_data_set.lineWidth = 2f
         g_data_set.color = Color.WHITE
         g_data_set.mode = LineDataSet.Mode.LINEAR
@@ -158,20 +236,20 @@ class PlotDelegateAdapterMy (
                 return true
             }
         })
-        selectChannel.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener{
-            override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
-                var set  = EMGChartLc.data.getDataSetByIndex(0)
-                var rez = EMGChartLc.data.removeDataSet(g_data_set)
-                if (isChecked)
-                    g_data_set.color = color
-                else
-                    g_data_set.color = Color.WHITE
-                EMGChartLc.data.addDataSet(g_data_set)
-                EMGChartLc.axisLeft.mAxisMaximum = 255f
-                Log.i("my tag", "${EMGChartLc.isAutoScaleMinMaxEnabled}")
-            }
-
-        })
+//        selectChannel.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener{
+//            override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+//                var set  = EMGChartLc.data.getDataSetByIndex(0)
+//                var rez = EMGChartLc.data.removeDataSet(g_data_set)
+//                if (isChecked)
+//                    g_data_set.color = color
+//                else
+//                    g_data_set.color = Color.WHITE
+//                EMGChartLc.data.addDataSet(g_data_set)
+//                EMGChartLc.axisLeft.mAxisMaximum = 255f
+//                Log.i("my tag", "${EMGChartLc.isAutoScaleMinMaxEnabled}")
+//            }
+//
+//        })
 
 
 
@@ -276,7 +354,50 @@ class PlotDelegateAdapterMy (
         set6.valueTextColor = Color.TRANSPARENT
         return set6
     }
+    private fun addEntrys(senses: MutableList<Int>, dataSets: MutableList<LineDataSet>, emgChart: LineChart) {
+        val data: LineData =  emgChart.data
 
+        for(i in 0..numberOfCharts-1){
+            if(dataSets[i].entryCount > 200){
+                main.runOnUiThread {
+                    dataSets[i].removeFirst()
+                }
+            }
+        }
+        main.runOnUiThread {
+
+            for(i in 0..<numberOfCharts){
+                dataSets[i].addEntry(Entry(count.toFloat(), senses[i].toFloat()))
+                data.removeDataSet(dataSets[i])
+                if(i == g_selected_chart)
+                    dataSets[i].color = main.getColor(R.color.ubi4_active)
+                else if(g_selected_chart == -1 )
+                    dataSets[i].color = Color.WHITE
+                else
+                    dataSets[i].color = Color.GRAY
+                data.addDataSet(dataSets[i])
+            }
+
+            invisible_top_data_set.addEntry(Entry(count.toFloat(), 255f))
+            invisible_bottom_data_set.addEntry(Entry(count.toFloat(), 0f))
+
+            data.removeDataSet(invisible_top_data_set)
+            data.addDataSet(invisible_top_data_set)
+            data.removeDataSet(invisible_bottom_data_set)
+            data.addDataSet(invisible_bottom_data_set)
+
+            emgChart.notifyDataSetChanged()
+
+            if (firstInit) {
+                emgChart.setVisibleXRangeMaximum(200f)
+                firstInit = false
+            }
+        }
+
+        emgChart.moveViewToX(count - 200.toFloat())
+        count += 1
+
+    }
     private fun addEntry(sens1: Int, emgChart: LineChart) {
         val data: LineData =  emgChart.data //binding.chartMainchart.data!!
 //        var set = data.getDataSetByIndex(0)
@@ -316,6 +437,7 @@ class PlotDelegateAdapterMy (
                 set0.removeFirst()
                 set1.removeFirst()
                 invisible_bottom_data_set.removeFirst()
+                invisible_top_data_set.removeFirst()
                 raw_data_set1 = raw_data_set1.subList(1,raw_data_set1.size)
 //                if (numberOfCharts >= 2) { set2.removeFirst() }
 //                if (numberOfCharts >= 3) { set3.removeFirst() }
@@ -424,13 +546,20 @@ class PlotDelegateAdapterMy (
 
 
     private suspend fun startGraphEnteringDataCoroutine(emgChart: LineChart)  {
-        dataSens1 += 1
+//        if (!firstInit) {
+            for (i in 0..numberOfCharts - 1) {
+                dataSenses[i] += i+1
+                dataSenses[i] %= 256
+            }
+            dataSens1 += 1
 //        dataSens2 += 1
 //        dataSens3 += 1
 //        dataSens4 += 2
 //        dataSens5 += 2
 //        dataSens6 += 2
-        if (dataSens1 > 255) { dataSens1 = 0 }
+            if (dataSens1 > 255) {
+                dataSens1 = 0
+            }
 //        if (dataSens2 > 255) { dataSens2 = 0 }
 //        if (dataSens3 > 255) { dataSens3 = 0 }
 //        if (dataSens4 > 255) { dataSens4 = 0 }
@@ -438,10 +567,11 @@ class PlotDelegateAdapterMy (
 //        if (dataSens6 > 255) { dataSens6 = 0 }
 
 //        addEntry(dataSens1, dataSens2, dataSens3, dataSens4, dataSens5, dataSens6, emgChart)
-        addEntry(dataSens1, emgChart)
-        delay(ConstantManager.GRAPH_UPDATE_DELAY.toLong())
-        if (graphThreadFlag) {
-            startGraphEnteringDataCoroutine(emgChart)
-        }
+            addEntrys(dataSenses, g_data_sets, emgChart)
+            delay(ConstantManager.GRAPH_UPDATE_DELAY.toLong())
+            if (graphThreadFlag) {
+                startGraphEnteringDataCoroutine(emgChart)
+            }
+//        }
     }
 }

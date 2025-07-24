cfc508bbb1aa3cb01180263d5111e3fdee6f7541
Evrei-XD <30876844+HakerFromRussia-XD@users.noreply.github.com>
2025-07-24 18:35:52 +0300

    исправление бага с рекурсивным вызовом вьюх на ios (не проваливаемся в подключение к устройству с экрана сканирования) iteration 4

--- iosApp/MotoricaStart/Presentation/CustomUI/CustomSlider.swift
+++ iosApp/MotoricaStart/Presentation/CustomUI/CustomSlider.swift
@@ -65,20 +65,25 @@ struct CustomSlider: View {
                 Circle()
                     .fill(Color.white)
                     .shadow(radius: 2)
-                    .frame(width: trackHeight, height: trackHeight)
-                    .offset(x: CGFloat((value - range.lowerBound) / (range.upperBound - range.lowerBound)) * geometry.size.width - trackHeight / 2)
+                    .frame(width: trackHeight, height: trackHeight)//это размеры пипки за которую тянем
+//                    .offset(x: CGFloat(value) - (geometry.size.width/2 - trackHeight / 2 - 4))
+                    .offset(x: -179)//0 - ((geometry.size.width/2) (CGFloat((value - range.lowerBound) / (range.upperBound - range.lowerBound)) * (geometry.size.width) - trackHeight / 2)) //- (geometry.size.width/2 - (trackHeight + 4)))
+                    //это положение пипки за которую тянем
                     .gesture(
                         DragGesture()
                             .onChanged { gesture in
-                                let relativeX = gesture.location.x / geometry.size.width // Нормализуем значение от 0 до 1 (от левого до правого края)
+                                let relativeX = gesture.location.x / (geometry.size.width - trackHeight - 8) // Нормализуем значение от -1 до 1 (от левого до правого края)
+                                let normalizedX = (relativeX + 1) / 2 // Преобразуем значение от -1..1 в 0..1
                                 // Преобразуем нормализованное значение в значение слайдера
-                                let newValue = Float(relativeX) * Float(range.upperBound - range.lowerBound) + Float(range.lowerBound)
+                                let newValue = Float(normalizedX) * Float(range.upperBound - range.lowerBound) + Float(range.lowerBound)
                                 // Ограничиваем значение слайдера в пределах диапазона
-                                value = min(max(newValue, Float(range.lowerBound)), Float(range.upperBound))
+                                value = newValue//max(Float(range.lowerBound), min(newValue + Float(trackHeight * 2), Float(range.upperBound)))
                                 print("========================================[test_slider]")
                                 print("[test_slider] relativeX = \(relativeX)")
+                                print("[test_slider] normalizedX = \(normalizedX)")
                                 print("[test_slider] gesture.location.x = \(gesture.location.x)")
                                 print("[test_slider] geometry.size.width = \(geometry.size.width)")
+                                print("[test_slider] trackHeight = \(trackHeight)")
                                 print("[test_slider] newValue = \(newValue)")
                                 print("[test_slider] range.upperBound = \(range.upperBound)")
                                 print("[test_slider] range.lowerBound = \(range.lowerBound)")
@@ -87,8 +92,8 @@ struct CustomSlider: View {
                     )
             }
             .padding(.top, 4)
-            .padding(.leading, trackHeight / 2 + 4)
-            .padding(.trailing, trackHeight / 2 + 4)
+//            .padding(.leading, trackHeight / 2 + 4)
+//            .padding(.trailing, trackHeight / 2 + 4)
         }
         .frame(height: trackHeight)
     }

d125ac579f3a50012398face06559a25858702da
Evrei-XD <30876844+HakerFromRussia-XD@users.noreply.github.com>
2025-09-09 04:34:04 +0300

    Добавление виджета графиков (iteration 10)

--- iosApp/MotoricaStart/Presentation/Widgets/Cells/PlotViewCell.swift
+++ iosApp/MotoricaStart/Presentation/Widgets/Cells/PlotViewCell.swift
@@ -6,7 +6,7 @@ final class PlotViewCell: UITableViewCell {
     static let reuseIdentifier = String(describing: PlotViewCell.self)
     static let height = CGFloat(130)
     private var viewModel: PlotListItemViewModel!
-    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var lineChartView: LineChartView?
     
     // charts
     let values = (0..<1).map { (i) -> ChartDataEntry in
@@ -25,10 +25,7 @@ final class PlotViewCell: UITableViewCell {
     }
     
     override func awakeFromNib() {
-        super.awakeFromNib()
-//        if lineChartView?.data == nil {
-//            lineChartView?.data = LineChartData()
-//        }
+//        super.awakeFromNib()
         initChart()
     }
     
@@ -38,13 +35,15 @@ final class PlotViewCell: UITableViewCell {
         selectionStyle = .none
         backgroundColor = UIColor(named: "ubi4_back")
         
-        lineChartView.data = viewModel.chartData
+        lineChartView?.data = viewModel.chartData
     }
     
     
     // MARK: - работа с графиком
     func addEntry (sens1: Int, sens2: Int) {
-        let data: ChartData = (self.lineChartView?.data!)!
+//        let data: ChartData = (self.lineChartView?.data!)!
+        guard let lineChartView = lineChartView,
+               let data = lineChartView.data else { return }
 
         var set1 : LineChartDataSet
         var set2 : LineChartDataSet
@@ -56,8 +55,12 @@ final class PlotViewCell: UITableViewCell {
             data.append(set2)
             firstInit = false
         } else {
-            set1 = (data[1] as? LineChartDataSet)!
-            set2 = (data[2] as? LineChartDataSet)!
+            guard
+                let existingSet1 = data[1] as? LineChartDataSet,
+                let existingSet2 = data[2] as? LineChartDataSet
+            else { return }
+            set1 = existingSet1
+            set2 = existingSet2
         }
         if (set1.count >= 300) {
             zaglushka(bool1: (set1.removeFirst()))
@@ -68,50 +71,51 @@ final class PlotViewCell: UITableViewCell {
         data.appendEntry(ChartDataEntry(x: Double(self.count), y: Double(sens2)), toDataSet: 2)
         
         data.notifyDataChanged()
-        self.lineChartView?.notifyDataSetChanged()
-        self.lineChartView?.setVisibleXRangeMaximum(300)
-        self.lineChartView?.moveViewToX(Double(set2.count - 300))
+        lineChartView.notifyDataSetChanged()
+        lineChartView.setVisibleXRangeMaximum(300)
+        lineChartView.moveViewToX(Double(set2.count - 300))
         self.count += 1
     }
     func initChart() {
-        self.lineChartView?.noDataText = "Нет данных"
-        self.lineChartView?.data = LineChartData()
-        var data = self.lineChartView?.data
+        guard let lineChartView = lineChartView else { return }
+        lineChartView.noDataText = "Нет данных"
+        lineChartView.data = LineChartData()
+        var data = lineChartView.data
         let set1 = LineChartDataSet(entries: [], label: "")
         data = LineChartData(dataSet: set1)
-        var data2 = self.lineChartView?.data
+        var data2 = lineChartView.data
         let set2 = LineChartDataSet(entries: [], label: "")
         data2 = LineChartData(dataSet: set2)
-        self.lineChartView?.data = data
-        self.lineChartView?.data = data2
+        lineChartView.data = data
+        lineChartView.data = data2
         
-        self.lineChartView?.isExclusiveTouch = false
-        self.lineChartView?.isMultipleTouchEnabled = false
-        self.lineChartView?.dragEnabled = false
-        self.lineChartView?.dragDecelerationEnabled = false
-        self.lineChartView?.setScaleEnabled(false)
-        self.lineChartView?.drawGridBackgroundEnabled = false
-        self.lineChartView?.pinchZoomEnabled = false
-        self.lineChartView?.backgroundColor = UIColor(named: "transparent") ?? .clear
+        lineChartView.isExclusiveTouch = false
+        lineChartView.isMultipleTouchEnabled = false
+        lineChartView.dragEnabled = false
+        lineChartView.dragDecelerationEnabled = false
+        lineChartView.setScaleEnabled(false)
+        lineChartView.drawGridBackgroundEnabled = false
+        lineChartView.pinchZoomEnabled = false
+        lineChartView.backgroundColor = UIColor(named: "transparent") ?? .clear
 
-        self.lineChartView?.legend.enabled = false
-        self.lineChartView?.animate(yAxisDuration: 0.7)
-        
-        let x: XAxis = self.lineChartView!.xAxis
+        lineChartView.legend.enabled = false
+        lineChartView.animate(yAxisDuration: 0.7)
+
+        let x: XAxis = lineChartView.xAxis
         x.labelTextColor = UIColor(named: "transparent") ?? .clear
         x.drawGridLinesEnabled = false
         x.axisMaximum = 4000000
         x.avoidFirstLastClippingEnabled = true
         
-        let y: YAxis = self.lineChartView!.leftAxis
+        let y: YAxis = lineChartView.leftAxis
         y.axisMaximum = 255
         y.axisMinimum = 0
         y.labelTextColor = UIColor(named: "transparent") ?? .clear
         y.drawGridLinesEnabled = true
         y.drawAxisLineEnabled = false
         y.gridColor = UIColor(named: "transparent") ?? .clear
-        self.lineChartView?.rightAxis.axisLineColor = UIColor(named: "transparent") ?? .clear
-        self.lineChartView?.rightAxis.labelTextColor = UIColor(named: "transparent") ?? .clear
+        lineChartView.rightAxis.axisLineColor = UIColor(named: "transparent") ?? .clear
+        lineChartView.rightAxis.labelTextColor = UIColor(named: "transparent") ?? .clear
     }
     func createSet1(values: [ChartDataEntry]) -> LineChartDataSet {
         let set1 = LineChartDataSet(entries: [], label: "")

2f151ab360aa5db51e9f232461b0389f48ea95f3
Denis <touron@yandex.ru>
2026-04-03 11:05:17 +0300

    коммит после мерджа

--- app/src/main/java/com/bailout/stickk/ubi4/adapters/widgetDelegateAdaptersV3/ToggleSliderDelegateAdapterV3.kt
+++ app/src/main/java/com/bailout/stickk/ubi4/adapters/widgetDelegateAdaptersV3/ToggleSliderDelegateAdapterV3.kt
@@ -115,7 +115,7 @@ class ToggleSliderDelegateAdapterV3(
         widgetInfoList.add(currentSliderInfo)
 
 
-//        indexWidgetSlidersArray = getIndexWidgetSlider(parameterInfo.dataCode)
+//      indexWidgetSlidersArray = getIndexWidgetSlider(parameterInfo.dataCode)
         sliderCollect()
 
 

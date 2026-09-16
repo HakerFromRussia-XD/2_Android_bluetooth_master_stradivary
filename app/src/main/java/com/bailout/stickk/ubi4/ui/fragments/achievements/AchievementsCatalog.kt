b8d47f36e402aebcf9de780e79f1a242770d22ba
Roman <30876844+HakerFromRussia-XD@users.noreply.github.com>
2026-09-16 17:21:58 +0300

    Use 256px transparent WebP achievement illustrations

--- app/src/main/java/com/bailout/stickk/ubi4/ui/fragments/achievements/AchievementsCatalog.kt
+++ app/src/main/java/com/bailout/stickk/ubi4/ui/fragments/achievements/AchievementsCatalog.kt
@@ -12,7 +12,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.BIONIC,
             iconRes = R.drawable.achievement_bionic,
-            artworkBounds = IntRect(0, 125, 1024, 899),
+            artworkBounds = IntRect(0, 30, 256, 226),
             titleRes = R.string.achievement_bionic_title,
             bronzeRes = R.string.achievement_bionic_bronze,
             silverRes = R.string.achievement_bionic_silver,
@@ -21,7 +21,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.CYBORG,
             iconRes = R.drawable.achievement_cyborg,
-            artworkBounds = IntRect(0, 0, 1024, 1024),
+            artworkBounds = IntRect(1, 0, 256, 256),
             titleRes = R.string.achievement_cyborg_title,
             bronzeRes = R.string.achievement_cyborg_bronze,
             silverRes = R.string.achievement_cyborg_silver,
@@ -30,7 +30,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.STREAK,
             iconRes = R.drawable.achievement_streak,
-            artworkBounds = IntRect(0, 0, 1024, 1024),
+            artworkBounds = IntRect(0, 0, 256, 256),
             titleRes = R.string.achievement_streak_title,
             bronzeRes = R.string.achievement_streak_bronze,
             silverRes = R.string.achievement_streak_silver,
@@ -39,7 +39,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.LONG_HAUL,
             iconRes = R.drawable.achievement_long_haul,
-            artworkBounds = IntRect(0, 202, 1024, 1024),
+            artworkBounds = IntRect(0, 49, 256, 256),
             titleRes = R.string.achievement_long_haul_title,
             bronzeRes = R.string.achievement_long_haul_bronze,
             silverRes = R.string.achievement_long_haul_silver,
@@ -48,7 +48,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.SCIENTIST,
             iconRes = R.drawable.achievement_scientist,
-            artworkBounds = IntRect(2, 0, 1252, 1254),
+            artworkBounds = IntRect(0, 7, 256, 256),
             titleRes = R.string.achievement_scientist_title,
             bronzeRes = R.string.achievement_scientist_bronze,
             silverRes = R.string.achievement_scientist_silver,
@@ -57,7 +57,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.DAILY_CHALLENGE,
             iconRes = R.drawable.achievement_daily_challenge,
-            artworkBounds = IntRect(0, 89, 1254, 1166),
+            artworkBounds = IntRect(0, 17, 256, 241),
             titleRes = R.string.achievement_daily_challenge_title,
             bronzeRes = R.string.achievement_daily_challenge_bronze,
             silverRes = R.string.achievement_daily_challenge_silver,
@@ -66,7 +66,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.PRECISION,
             iconRes = R.drawable.achievement_precision,
-            artworkBounds = IntRect(9, 21, 1229, 1254),
+            artworkBounds = IntRect(0, 18, 253, 236),
             titleRes = R.string.achievement_precision_title,
             bronzeRes = R.string.achievement_precision_bronze,
             silverRes = R.string.achievement_precision_silver,
@@ -75,7 +75,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.POWER,
             iconRes = R.drawable.achievement_power,
-            artworkBounds = IntRect(1, 0, 1254, 1253),
+            artworkBounds = IntRect(0, 0, 256, 256),
             titleRes = R.string.achievement_power_title,
             bronzeRes = R.string.achievement_power_bronze,
             silverRes = R.string.achievement_power_silver,
@@ -84,7 +84,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.GET_A_GRIP,
             iconRes = R.drawable.achievement_get_a_grip,
-            artworkBounds = IntRect(0, 0, 1254, 1254),
+            artworkBounds = IntRect(0, 0, 256, 256),
             titleRes = R.string.achievement_get_a_grip_title,
             bronzeRes = R.string.achievement_get_a_grip_bronze,
             silverRes = R.string.achievement_get_a_grip_silver,
@@ -93,7 +93,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.ALTER_EGO,
             iconRes = R.drawable.achievement_alter_ego,
-            artworkBounds = IntRect(0, 20, 1022, 1018),
+            artworkBounds = IntRect(30, 3, 245, 254),
             titleRes = R.string.achievement_alter_ego_title,
             bronzeRes = R.string.achievement_alter_ego_bronze,
             silverRes = R.string.achievement_alter_ego_silver,
@@ -102,7 +102,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.ANNIVERSARY,
             iconRes = R.drawable.achievement_anniversary,
-            artworkBounds = IntRect(0, 0, 1242, 1254),
+            artworkBounds = IntRect(0, 0, 256, 256),
             titleRes = R.string.achievement_anniversary_title,
             bronzeRes = R.string.achievement_anniversary_bronze,
             silverRes = R.string.achievement_anniversary_silver,
@@ -118,7 +118,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.PERSONALISATION,
             iconRes = R.drawable.achievement_personalisation,
-            artworkBounds = IntRect(13, 179, 1241, 1089),
+            artworkBounds = IntRect(1, 34, 255, 224),
             titleRes = R.string.achievement_personalisation_title,
             bronzeRes = R.string.achievement_personalisation_bronze,
             silverRes = R.string.achievement_personalisation_silver,
@@ -127,7 +127,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.ALWAYS_CONNECTED,
             iconRes = R.drawable.achievement_phone,
-            artworkBounds = IntRect(0, 9, 1254, 1244),
+            artworkBounds = IntRect(0, 0, 256, 256),
             titleRes = R.string.achievement_always_connected_title,
             bronzeRes = R.string.achievement_always_connected_bronze,
             silverRes = R.string.achievement_always_connected_silver,
@@ -136,7 +136,7 @@ object AchievementsCatalog {
         achievement(
             id = AchievementId.CHAMPION,
             iconRes = R.drawable.achievement_champion,
-            artworkBounds = IntRect(17, 4, 1237, 1219),
+            artworkBounds = IntRect(1, 0, 255, 251),
             titleRes = R.string.achievement_champion_title,
             bronzeRes = R.string.achievement_champion_bronze,
             silverRes = R.string.achievement_champion_silver,
